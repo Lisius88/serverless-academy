d8130afd97215d81815ffe6e8435a8bcb68f45bc
Makarov Yelisei <fenniz123@gmail.com>
2023-02-22 18:39:05 +0200

    fix

--- 03_cli_telegram_console_sender/app.js
+++ 03_cli_telegram_console_sender/app.js
@@ -1,78 +1,60 @@
-const {program} = require("commander")
-const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs/promises');
-const path = require('path');
+import TelegramBot from "node-telegram-bot-api";
+import path from 'path';
+import * as fs from 'fs';
+import * as url from 'url';
+import {program} from 'commander'
+import { exit } from "process";
 
-const token = '5889077740:AAEJDXPTjbygcsX7JPucs8ut5XyhRMLPBrM';
-const bot = new TelegramBot(token, {polling: true});
 
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const filePath = path.join(__dirname, 'id.txt')
+const filePathSecond = path.join(__dirname, 'token.txt')
+const TOKEN = fs.readFileSync(filePathSecond)
 
-const exit =  () => {
-    process.exit(1)
-}
+const bot = new TelegramBot(JSON.parse(TOKEN), { polling: true });
 
-const sendMessage = async ({ message }) => {
-    try {
+program
+  .command('send-message <message>')
+  .description('Send a message to your telegram bot')
+    .action(async (message) => {
     bot.on('message', async (msg) => {
-    let chatId =  msg.chat.id;
-        if (msg.text === '/start') {
-            await fs.writeFile(filePath, chatId.toString());
-            exit()
+    const chatId = msg.chat.id;
+    fs.writeFileSync(filePath, chatId.toString())
+    if (msg.text === '/start') {
+    await bot.sendMessage(chatId, message)
+    exit()
     }
-    })
-    if (message) {
-    let userId = await fs.readFile(filePath)
-        await bot.sendMessage(userId, message)
-        console.log(message)
-        exit()
+    });
+    const id = fs.readFileSync(filePath)
+    if (id.toString() && id.toString() !== undefined && id.toString() !== null) {
+    await bot.sendMessage(id, message);
+    return exit()
     }
-    else return
-    } catch (error) {
-    console.log(error.message)    
+    });
+  
+program
+  .command('send-photo <path>')
+  .description('Send a photo to your telegram bot')
+    .action(async (path) => {
+    bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    fs.writeFileSync(filePath, chatId.toString())
+    if (msg.text === '/start') {
+    await bot.sendPhoto(chatId, path)
+    exit()
     }
-}
-
-const sendPhoto = async ({ photo }) => {
-    try {
-    if (photo) {
-    let userId = await fs.readFile(filePath)
-        await bot.sendPhoto(userId, photo);
-        exit()
+    });
+    const id = fs.readFileSync(filePath)
+     if (id.toString() && id.toString() !== undefined && id.toString() !== null) {
+    await bot.sendPhoto(id, path);
+    exit()    
     }
-    else return
-    } catch (error) {
-    console.log(error.message)    
-    }
-}
-
-const getInfo = async ({ help }) => {
-   try {
-       if (help) {
-           const flags = program.options.map(option => option.flags)
-           const description = program.options.map(option => option.description)
-           console.log(`Options:
-1) ${flags[0]} ${description[0]}
-2) ${flags[1]} ${description[1]}
-3) ${flags[2]} ${description[2]}
-           `)
-           exit()
-       }
-    else return
-    } catch (error) {
-    console.log(error.message)    
-    }
-}
+  });
 
 
 program
-    .option("-m --message <message>", "Send message to user")
-    .option("-p --photo <path>", "Send photo to user")
     .option("-h --help", "Display help for command")
 
 program.parse(process.argv)
-const options = program.opts()
 
-sendMessage(options)
-sendPhoto(options)
-getInfo(options)
+
