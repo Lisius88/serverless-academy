bf4f7b1e3e34084f3f1562d733637afabec63c0b
Makarov Yelisei <fenniz123@gmail.com>
2023-03-05 19:04:17 +0200

    fix

--- 05_telegram_bot_exchange_rates/app.js
+++ 05_telegram_bot_exchange_rates/app.js
@@ -20,72 +20,15 @@ const forecastWithInterval = async (id) => {
     try {
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=41.390205&lon=2.154007&units=metric&appid=${APIKEY}`)  
         const data = response.data.list;
-        const newData = data.splice(1, 9)
-        await bot.sendMessage(id, `
-${newData[0].dt_txt}
-${newData[0].weather[0].main} ${Math.round(newData[0].main.temp)}℃
-Feels like: ${Math.round(newData[0].main.feels_like)}℃
-Min: ${Math.round(newData[0].main.temp_min)}℃                              
-Max: ${Math.round(newData[0].main.temp_max)}℃                             
-Pressure: ${newData[0].main.pressure}                  
-Humidity: ${newData[0].main.humidity}%                
-
-${newData[1].dt_txt}
-${newData[1].weather[0].main} ${Math.round(newData[1].main.temp)}℃
-Feels like: ${Math.round(newData[1].main.feels_like)}℃
-Min: ${Math.round(newData[1].main.temp_min)}℃
-Max: ${Math.round(newData[1].main.temp_max)}℃
-Pressure: ${newData[1].main.pressure}
-Humidity: ${newData[1].main.humidity}%
-
-${newData[2].dt_txt}
-${newData[2].weather[0].main} ${Math.round(newData[2].main.temp)}℃
-Feels like: ${Math.round(newData[3].main.feels_like)}℃  
-Min: ${Math.round(newData[2].main.temp_min)}℃ 
-Max: ${Math.round(newData[2].main.temp_max)}℃ 
-Pressure: ${newData[2].main.pressure} 
-Humidity: ${newData[2].main.humidity}%  
-
-${newData[3].dt_txt}
-${newData[3].weather[0].main} ${Math.round(newData[3].main.temp)}℃
-Feels like: ${Math.round(newData[3].main.feels_like)}℃
-Min: ${Math.round(newData[3].main.temp_min)}℃
-Max: ${Math.round(newData[3].main.temp_max)}℃
-Pressure: ${newData[3].main.pressure}
-Humidity: ${newData[3].main.humidity}%
-
-${newData[4].dt_txt}
-${newData[4].weather[0].main} ${Math.round(newData[4].main.temp)}℃
-Feels like: ${Math.round(newData[4].main.feels_like)}℃
-Min: ${Math.round(newData[4].main.temp_min)}℃ 
-Max: ${Math.round(newData[4].main.temp_max)}℃ 
-Pressure: ${newData[4].main.pressure}
-Humidity: ${newData[4].main.humidity}%
-
-${newData[5].dt_txt}
-${newData[5].weather[0].main} ${Math.round(newData[5].main.temp)}℃
-Feels like: ${Math.round(newData[5].main.feels_like)}℃
-Min: ${Math.round(newData[5].main.temp_min)}℃
-Max: ${Math.round(newData[5].main.temp_max)}℃
-Pressure: ${newData[5].main.pressure}
-Humidity: ${newData[5].main.humidity}%
-
-${newData[6].dt_txt}
-${newData[6].weather[0].main} ${Math.round(newData[6].main.temp)}℃
-Feels like: ${Math.round(newData[6].main.feels_like)}℃
-Min: ${Math.round(newData[6].main.temp_min)}℃
-Max: ${Math.round(newData[6].main.temp_max)}℃
-Pressure: ${newData[6].main.pressure}
-Humidity: ${newData[6].main.humidity}%
-
-${newData[7].dt_txt}
-${newData[7].weather[0].main} ${Math.round(newData[7].main.temp)}℃
-Feels like: ${Math.round(newData[7].main.feels_like)}℃
-Min: ${Math.round(newData[7].main.temp_min)}℃
-Max: ${Math.round(newData[7].main.temp_max)}℃
-Pressure: ${newData[7].main.pressure}
-Humidity: ${newData[7].main.humidity}%
-`);    
+        const message = data.map(data => `${data.dt_txt} ${data.weather[0].main} ${Math.round(data.main.temp)}℃ 
+Feels like:${Math.round(data.main.feels_like)}℃ 
+Min: ${Math.round(data.main.temp_min)}℃ 
+Max: ${Math.round(data.main.temp_max)}℃
+Pressure: ${data.main.pressure} 
+Humidity: ${data.main.humidity}%
+`)
+            .join('\n');
+        await bot.sendMessage(id, message);    
     } catch (error) {
     console.log(error)
     }
@@ -95,40 +38,16 @@ const forecastWithIntervalSecond = async (id) => {
     try {
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=41.390205&lon=2.154007&units=metric&appid=${APIKEY}`)  
         const data = response.data.list;
-        const newData = data.splice(2, 10)
-        await bot.sendMessage(id, `
-${newData[0].dt_txt}
-${newData[0].weather[0].main} ${Math.round(newData[0].main.temp)}℃
-Feels like: ${Math.round(newData[0].main.feels_like)}℃
-Min: ${Math.round(newData[0].main.temp_min)}℃
-Max: ${Math.round(newData[0].main.temp_max)}℃
-Pressure: ${newData[0].main.pressure}
-Humidity: ${newData[0].main.humidity}%
-
-${newData[2].dt_txt}
-${newData[2].weather[0].main} ${Math.round(newData[2].main.temp)}℃
-Feels like: ${Math.round(newData[2].main.feels_like)}℃
-Min: ${Math.round(newData[2].main.temp_min)}℃
-Max: ${Math.round(newData[2].main.temp_max)}℃
-Pressure: ${newData[2].main.pressure}
-Humidity: ${newData[2].main.humidity}%
-
-${newData[4].dt_txt}
-${newData[4].weather[0].main} ${Math.round(newData[4].main.temp)}℃
-Feels like: ${Math.round(newData[4].main.feels_like)}℃
-Min: ${Math.round(newData[4].main.temp_min)}℃
-Max: ${Math.round(newData[4].main.temp_max)}℃
-Pressure: ${newData[4].main.pressure}
-Humidity: ${newData[4].main.humidity}%
-
-${newData[6].dt_txt}
-${newData[6].weather[0].main} ${Math.round(newData[6].main.temp)}℃
-Feels like: ${Math.round(newData[6].main.feels_like)}℃
-Min: ${Math.round(newData[6].main.temp_min)}℃
-Max: ${Math.round(newData[6].main.temp_max)}℃
-Pressure: ${newData[6].main.pressure}
-Humidity: ${newData[6].main.humidity}%
-            `);    
+        const message = data.filter((data, index) => index % 2 == 0)
+            .map(data => `${data.dt_txt} ${data.weather[0].main} ${Math.round(data.main.temp)}℃ 
+Feels like:${Math.round(data.main.feels_like)}℃ 
+Min: ${Math.round(data.main.temp_min)}℃ 
+Max: ${Math.round(data.main.temp_max)}℃
+Pressure: ${data.main.pressure} 
+Humidity: ${data.main.humidity}%
+`)
+            .join('\n');
+    await bot.sendMessage(id, message);    
     } catch (error) {
     console.log(error)
     }
@@ -160,8 +79,6 @@ Wind degrees: ${deg}deg
 Wind gust: ${gust}m/s
             `);
 }
-
-
 
 const dollarRate = async (id) => {
     try {
@@ -243,6 +160,7 @@ Purchase: EUR = ${eur.rateBuy.toFixed(2)}UAH
     await bot.sendMessage(id, `${newDate}
 PrivatBank
 Sale: EUR = ${eurSellPrivat}UAH
+
 Purchase: EUR = ${eurBuyPrivat}UAH
 
 MonoBank
@@ -254,17 +172,13 @@ Purchase: EUR = ${eurBuyFromCache}UAH
 
 
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-  if (msg.text === '/start') {
-  bot.sendMessage(chatId, "Hi! What do you want to see?", opts[2])
-  }
-});
-
-bot.on('message', (msg) => {
     const chatId = msg.chat.id
     const text = msg.text
-    
+
     switch (text) {
+        case "/start":
+        bot.sendMessage(chatId, "Hi! What do you want to see?", opts[2])    
+            break;
         case "/Eschange rates":
         bot.sendMessage(chatId, "Choose the currency!", opts[0])    
             break;
