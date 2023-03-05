af5f42d8961d3723f6c7b6a5ff085e7dfdc4abfb
Makarov Yelisei <fenniz123@gmail.com>
2023-03-05 15:58:28 +0200

    fix

--- 04_telegram_bot_weather_forecast/app.js
+++ 04_telegram_bot_weather_forecast/app.js
@@ -15,72 +15,14 @@ const forecastWithInterval = async (id) => {
     try {
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=41.390205&lon=2.154007&units=metric&appid=${APIKEY}`)  
         const data = response.data.list;
-        const newData = data.splice(1, 9)
-        await bot.sendMessage(id, `
-${newData[0].dt_txt}
-${newData[0].weather[0].main} ${Math.round(newData[0].main.temp)}℃
-Feels like: ${Math.round(newData[0].main.feels_like)}℃
-Min: ${Math.round(newData[0].main.temp_min)}℃                              
-Max: ${Math.round(newData[0].main.temp_max)}℃                             
-Pressure: ${newData[0].main.pressure}                  
-Humidity: ${newData[0].main.humidity}%                
-
-${newData[1].dt_txt}
-${newData[1].weather[0].main} ${Math.round(newData[1].main.temp)}℃
-Feels like: ${Math.round(newData[1].main.feels_like)}℃
-Min: ${Math.round(newData[1].main.temp_min)}℃
-Max: ${Math.round(newData[1].main.temp_max)}℃
-Pressure: ${newData[1].main.pressure}
-Humidity: ${newData[1].main.humidity}%
-
-${newData[2].dt_txt}
-${newData[2].weather[0].main} ${Math.round(newData[2].main.temp)}℃
-Feels like: ${Math.round(newData[3].main.feels_like)}℃  
-Min: ${Math.round(newData[2].main.temp_min)}℃ 
-Max: ${Math.round(newData[2].main.temp_max)}℃ 
-Pressure: ${newData[2].main.pressure} 
-Humidity: ${newData[2].main.humidity}%  
-
-${newData[3].dt_txt}
-${newData[3].weather[0].main} ${Math.round(newData[3].main.temp)}℃
-Feels like: ${Math.round(newData[3].main.feels_like)}℃
-Min: ${Math.round(newData[3].main.temp_min)}℃
-Max: ${Math.round(newData[3].main.temp_max)}℃
-Pressure: ${newData[3].main.pressure}
-Humidity: ${newData[3].main.humidity}%
-
-${newData[4].dt_txt}
-${newData[4].weather[0].main} ${Math.round(newData[4].main.temp)}℃
-Feels like: ${Math.round(newData[4].main.feels_like)}℃
-Min: ${Math.round(newData[4].main.temp_min)}℃ 
-Max: ${Math.round(newData[4].main.temp_max)}℃ 
-Pressure: ${newData[4].main.pressure}
-Humidity: ${newData[4].main.humidity}%
-
-${newData[5].dt_txt}
-${newData[5].weather[0].main} ${Math.round(newData[5].main.temp)}℃
-Feels like: ${Math.round(newData[5].main.feels_like)}℃
-Min: ${Math.round(newData[5].main.temp_min)}℃
-Max: ${Math.round(newData[5].main.temp_max)}℃
-Pressure: ${newData[5].main.pressure}
-Humidity: ${newData[5].main.humidity}%
-
-${newData[6].dt_txt}
-${newData[6].weather[0].main} ${Math.round(newData[6].main.temp)}℃
-Feels like: ${Math.round(newData[6].main.feels_like)}℃
-Min: ${Math.round(newData[6].main.temp_min)}℃
-Max: ${Math.round(newData[6].main.temp_max)}℃
-Pressure: ${newData[6].main.pressure}
-Humidity: ${newData[6].main.humidity}%
-
-${newData[7].dt_txt}
-${newData[7].weather[0].main} ${Math.round(newData[7].main.temp)}℃
-Feels like: ${Math.round(newData[7].main.feels_like)}℃
-Min: ${Math.round(newData[7].main.temp_min)}℃
-Max: ${Math.round(newData[7].main.temp_max)}℃
-Pressure: ${newData[7].main.pressure}
-Humidity: ${newData[7].main.humidity}%
-`);    
+        const message = data.map(data => `${data.dt_txt} ${data.weather[0].main} ${Math.round(data.main.temp)}℃ 
+Feels like:${Math.round(data.main.feels_like)}℃ 
+Min: ${Math.round(data.main.temp_min)}℃ 
+Max: ${Math.round(data.main.temp_max)}℃
+Pressure: ${data.main.pressure} 
+Humidity: ${data.main.humidity}%`)
+            .join('\n');
+        await bot.sendMessage(id, message);    
     } catch (error) {
     console.log(error)
     }
@@ -90,40 +32,15 @@ const forecastWithIntervalSecond = async (id) => {
     try {
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=41.390205&lon=2.154007&units=metric&appid=${APIKEY}`)  
         const data = response.data.list;
-        const newData = data.splice(2, 10)
-        await bot.sendMessage(id, `
-${newData[0].dt_txt}
-${newData[0].weather[0].main} ${Math.round(newData[0].main.temp)}℃
-Feels like: ${Math.round(newData[0].main.feels_like)}℃
-Min: ${Math.round(newData[0].main.temp_min)}℃
-Max: ${Math.round(newData[0].main.temp_max)}℃
-Pressure: ${newData[0].main.pressure}
-Humidity: ${newData[0].main.humidity}%
-
-${newData[2].dt_txt}
-${newData[2].weather[0].main} ${Math.round(newData[2].main.temp)}℃
-Feels like: ${Math.round(newData[2].main.feels_like)}℃
-Min: ${Math.round(newData[2].main.temp_min)}℃
-Max: ${Math.round(newData[2].main.temp_max)}℃
-Pressure: ${newData[2].main.pressure}
-Humidity: ${newData[2].main.humidity}%
-
-${newData[4].dt_txt}
-${newData[4].weather[0].main} ${Math.round(newData[4].main.temp)}℃
-Feels like: ${Math.round(newData[4].main.feels_like)}℃
-Min: ${Math.round(newData[4].main.temp_min)}℃
-Max: ${Math.round(newData[4].main.temp_max)}℃
-Pressure: ${newData[4].main.pressure}
-Humidity: ${newData[4].main.humidity}%
-
-${newData[6].dt_txt}
-${newData[6].weather[0].main} ${Math.round(newData[6].main.temp)}℃
-Feels like: ${Math.round(newData[6].main.feels_like)}℃
-Min: ${Math.round(newData[6].main.temp_min)}℃
-Max: ${Math.round(newData[6].main.temp_max)}℃
-Pressure: ${newData[6].main.pressure}
-Humidity: ${newData[6].main.humidity}%
-            `);    
+        const message = data.filter((data, index) => index % 2 == 0)
+            .map(data => `${data.dt_txt} ${data.weather[0].main} ${Math.round(data.main.temp)}℃ 
+Feels like:${Math.round(data.main.feels_like)}℃ 
+Min: ${Math.round(data.main.temp_min)}℃ 
+Max: ${Math.round(data.main.temp_max)}℃
+Pressure: ${data.main.pressure} 
+Humidity: ${data.main.humidity}%`)
+            .join('\n');
+    await bot.sendMessage(id, message);    
     } catch (error) {
     console.log(error)
     }
